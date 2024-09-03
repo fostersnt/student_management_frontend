e43f5e389db264f7b2d5558a13d8861ccaadc971
Foster Amponsah Asante <fostersnt@gmail.com>
2024-09-03 10:52:12 +0000

    Project start-up

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data using axios
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    axios.get('http://localhost/student_management/back-end/trial.php')
+      .then(response => {
+        // Set the data in state
+        setData(response.data);
+        console.log(response);
+        
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
